f7b9e89ec90b26067daa7823e51b8802d6cca4b9
mythily-muthu <mythilimuthu18@gmail.com>
2023-06-06 15:03:41 +0530

    Fix: Hover effect added to active navbar

--- src/ components/Navbar.jsx
+++ src/ components/Navbar.jsx
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const [activeState, setActiveState] = useState("Home");
-  let navigate = useNavigate();
+
   let navList = [
     {
       title: "Home",
@@ -30,11 +29,7 @@ const Navbar = () => {
       link: "/contact",
     },
   ];
-  const handleNavList = (e, item) => {
-    e.preventDefault();
-    setActiveState(item.link);
-    navigate(item.link);
-  };
+
   return (
     <div className="flex h-14 md:h-16 lg:h-20 w-full items-center justify-between px-44 shadow-lg">
       <div className="flex cursor-pointer items-center w-11 h-11">
@@ -44,12 +39,18 @@ const Navbar = () => {
           className="h-full w-full object-contain"
         />
       </div>
-      <div className="flex gap-7 cursor-pointer items-center text-black text-base md:text-lg lg:text-xl ">
+      <div className="flex gap-7 cursor-pointer items-center text-black text-base md:text-lg lg:text-xl  ">
         {navList.map((item) => {
           return (
             <p
-              className={`${activeState === item.title && text- }`}
-              onClick={handleNavList}
+              key={item.title}
+              className={` ${
+                activeState === item.link && "text-primary underline"
+              }`}
+              onClick={(e) => {
+                e.preventDefault();
+                setActiveState(item.link);
+              }}
             >
               {item.title}
             </p>
